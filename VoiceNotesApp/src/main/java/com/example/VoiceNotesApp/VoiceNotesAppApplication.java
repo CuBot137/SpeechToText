cfade49a021efbbddb579eded922af2123e711a9
ConorLynam <conorlynam1234@gmail.com>
2023-10-22 17:23:08 +0100

    First Commit

---
+++ VoiceNotesApp/src/main/java/com/example/VoiceNotesApp/VoiceNotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.VoiceNotesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceNotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceNotesAppApplication.class, args);
+	}
+
+}
---
+++ VoiceNotesApp/src/test/java/com/example/VoiceNotesApp/VoiceNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VoiceNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
